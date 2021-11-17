4460deee7d961ea64e9b7d96c412363840bc46d8
Ryan Vieira <notgecko@pm.me>
2021-11-16 21:54:26 -0300

    Graphql, mongoose and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ProvidersModule } from './providers/providers.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { QuotesModule } from './quotes/quotes.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      debug: false,
+      playground: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ProvidersModule,
+    QuotesModule,
+  ],
 })
 export class AppModule {}
